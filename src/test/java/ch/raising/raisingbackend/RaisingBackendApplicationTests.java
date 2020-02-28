c8879cfc2baf47ca394f591c52bd20c94d60857b
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2020-02-28 10:43:46 +0100

    Setup Spring

---
+++ src/test/java/ch/raising/raisingbackend/RaisingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.raising.raisingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaisingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
