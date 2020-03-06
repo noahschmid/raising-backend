cd25008a8e7abb924f8ec1d41271317bbddb3096
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2020-03-06 08:44:46 +0100

    Add V1.3 V2

---
+++ src/main/resources/db/migration/V1.3__alter_startup.sql
@@ -0,0 +1,8 @@
+ALTER TABLE startup
+ADD (numberOfFTE int,
+	turnover int,
+	street varchar,
+	city varchar,
+	website varchar, 
+	breakevenYear int,
+	zipCode int);
---
+++ src/main/resources/db/migration/V2__add_startup_specific_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE contact (
+	id serial PRIMARY KEY,
+	startupId int REFERENCES startup(id),
+	name varchar,
+	role varchar,
+	email varchar,
+	phone varchar
+);
+CREATE TABLE boardmember (
+	id serial PRIMARY KEY,
+	startupId REFERENCES startup(id),
+	name varchar,
+	education varchar,
+	profession varchar,
+	pullDownType varchar,
+	PullDownDuration int
+);
+CREATE TABLE founder (
+	id serial PRIMARY KEY, 
+	startipId int REFERENCES startup(id),
+	name varchar,
+	role varchar,
+	education varchar
+);
