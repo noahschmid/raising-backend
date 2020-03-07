983f853d71566197d54344875b467820de8e79b4
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2020-03-07 10:51:45 +0100

    Test ssl connection

--- src/main/java/ch/raising/config/ServerConfig.java
+++ src/main/java/ch/raising/config/ServerConfig.java
@@ -42,7 +42,7 @@ public class ServerConfig {
 	private Connector getHttpConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
-		connector.setPort(8082);
+		connector.setPort(8080);
 		connector.setSecure(false);
 		connector.setRedirectPort(8443);
 		return connector;
