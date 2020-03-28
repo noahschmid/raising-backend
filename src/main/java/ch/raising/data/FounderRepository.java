9fa4fa021e335d9c4240adfd4cae24777e47bf1b
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2020-03-28 12:53:19 +0100

    Update models

--- src/main/java/ch/raising/data/FounderRepository.java
+++ src/main/java/ch/raising/data/FounderRepository.java
@@ -40,7 +40,7 @@ public class FounderRepository implements IAdditionalInformationRepository<Found
 	@Override
 	public void addMemberByStartupId(Founder founder, long accountId) {
 		jdbc.execute(
-				"INSERT INTO founder(startupid, firstname, lastname, education, position) VALUES (?,?,?,?,?)",
+				"INSERT INTO founder(startupid, firstname, lastname, education, position, countryid) VALUES (?, ?,?,?,?,?)",
 				addByStartupId(founder, accountId));
 	}
 
@@ -52,7 +52,7 @@ public class FounderRepository implements IAdditionalInformationRepository<Found
 	@Override
 	public Founder mapRowToModel(ResultSet rs, int row) throws SQLException {
 		return Founder.builder().id(rs.getLong("id")).startupid(rs.getLong("startupid"))
-				.firstName(rs.getString("firstname")).lastName(rs.getString("lastname")).position(rs.getString("position"))
+				.firstName(rs.getString("firstname")).lastName(rs.getString("lastname")).position(rs.getString("position")).countryId(rs.getLong("countryid"))
 				.education(rs.getString("education")).build();
 	}
 
@@ -78,6 +78,7 @@ public class FounderRepository implements IAdditionalInformationRepository<Found
 				ps.setString(c++, founder.getLastName());
 				ps.setString(c++, founder.getEducation());
 				ps.setString(c++, founder.getPosition());
+				ps.setLong(c++, founder.getCountryId());
 				return ps.execute();
 			}
 		};
