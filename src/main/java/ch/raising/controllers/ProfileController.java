38cd9d5458e0613c1af097eb84b6d0d4e1ea5215
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2020-04-13 09:24:23 +0200

    Reintroduce /profile/*/* endpoints for frontend

--- src/main/java/ch/raising/controllers/ProfileController.java
+++ src/main/java/ch/raising/controllers/ProfileController.java
@@ -4,6 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.dao.DataAccessException;
+
+import java.sql.SQLException;
 
 import ch.raising.services.InvestorService;
 import ch.raising.services.StartupService;
@@ -19,6 +28,16 @@ public class ProfileController {
 	public ProfileController(StartupService suService, InvestorService invService) {
 		this.suService = suService;
 		this.invService = invService;
+	}
+
+	@GetMapping("/startup/{id}")
+	public ResponseEntity<?> getStartup(@PathVariable long id) throws DataAccessException, SQLException{
+		return ResponseEntity.ok(suService.getProfile(id));
+	}
+	
+	@GetMapping("/investor/{id}")
+	public ResponseEntity<?> getInvestor(@PathVariable long id)throws DataAccessException, SQLException{
+		return ResponseEntity.ok(invService.getProfile(id));
 	}
 	
 }
