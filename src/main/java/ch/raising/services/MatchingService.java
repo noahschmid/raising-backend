18f57ff30ce684e777806432de6493f407288fb9
noahschmid <noah.schmid77@gmail.com>
2020-04-16 16:01:33 +0200

    Change encoding to UTF-8 and change match accept logic

--- src/main/java/ch/raising/services/MatchingService.java
+++ src/main/java/ch/raising/services/MatchingService.java
@@ -193,6 +193,16 @@ public class MatchingService {
                     relationshipRepository.updateState(id, RelationshipState.HANDSHAKE);
             break;
 
+            case STARTUP_DECLINED: 
+                if(isStartup)
+                    relationshipRepository.updateState(id, RelationshipState.STARTUP_ACCEPTED);
+            break;
+
+            case INVESTOR_DECLINED:
+                if(!isStartup)
+                    relationshipRepository.updateState(id, RelationshipState.INVESTOR_ACCEPTED);
+            break;
+
             default:
             break;
         }
