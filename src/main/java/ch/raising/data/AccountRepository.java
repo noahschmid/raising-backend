4761f4ead6f4925f73556a6a5e71391436fae6d8
noahschmid <noah.schmid77@gmail.com>
2020-04-18 13:27:36 +0200

    Fix rowmapper in account

--- src/main/java/ch/raising/data/AccountRepository.java
+++ src/main/java/ch/raising/data/AccountRepository.java
@@ -163,7 +163,7 @@ public class AccountRepository implements IRepository<Account> {
 				.pitch(rs.getString("pitch")).description(rs.getString("description")).email(rs.getString("emailHash"))
 				.roles(rs.getString("roles")).ticketMaxId(rs.getInt("ticketmaxid"))
 				.ticketMinId(rs.getInt("ticketminid")).password(rs.getString("password"))
-				.countryId(rs.getObject("countryId") == null ? null : rs.getLong("countryId"))
+				.countryId(rs.getLong("countryId"))
 				.website(rs.getString("website"))
 				.profilePictureId(rs.getObject("profilepictureid") == null ?
 				-1 : rs.getLong("profilepictureid")).build();
