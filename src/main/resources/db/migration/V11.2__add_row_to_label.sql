57dca6a76fe9fb42593eb34f6dc115a24d4c49a1
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2020-05-05 13:17:32 +0200

    Add flyway migration

--- src/main/resources/db/migration/V11.2__add_row_to_label.sql
+++ src/main/resources/db/migration/V11.2__add_row_to_label.sql
@@ -1,1 +1,12 @@
 ALTER TABLE label ADD COLUMN IF NOT EXISTS iconid int REFERENCES icon(id) ON DELETE SET NULL;
+
+ALTER TABLE relationship ADD COLUMN IF NOT EXISTS investorDecidedAt timestamp;
+ALTER TABLE relationship ADD COLUMN IF NOT EXISTS startupDecidedAt timestamp;
+
+ALTER TABLE share DROP CONSTRAINT IF EXISTS share_accountid_fkey;
+ALTER TABLE share ADD CONSTRAINT share_accountid_fkey FOREIGN KEY (accountid) REFERENCES account(id) ON DELETE CASCADE;
+
+ALTER TABLE interaction DROP CONSTRAINT IF EXISTS interaction_investorid_fkey;
+ALTER TABLE interaction DROP CONSTRAINT IF EXISTS interaction_startupid_fkey;
+ALTER TABLE interaction ADD CONSTRAINT interaction_investorid_fkey FOREIGN KEY (investorid) REFERENCES investor(accountid) ON DELETE CASCADE;
+ALTER TABLE interaction ADD CONSTRAINT interaction_startup_fkey FOREIGN KEY (startupid) REFERENCES startup(accountid) ON DELETE CASCADE;
