f74fc952c6015e4e62ca88d3d6ff07880720f3bd
noahschmid <noah.schmid77@gmail.com>
2020-05-16 13:33:55 +0200

    Fix UidUtil

--- src/main/java/ch/raising/utils/UidUtil.java
+++ src/main/java/ch/raising/utils/UidUtil.java
@@ -5,23 +5,38 @@ package ch.raising.utils;
  *
  */
 public class UidUtil {
-	
-	public static boolean isValidUId(String unternehmensId) {
+	/**
+     * Check whether given uid is a valid one
+     *
+     * @param uid
+     * @return
+     */
+    public static boolean isValidUid(String uid) {
+        if (!uid.matches("[A-Z]{3}-\\d\\d\\d\\.\\d\\d\\d\\.\\d\\d\\d")) {
+            return false;
+        }
 
-//		if(unternehmensId.length() < 13)
-//			return false;
-//		char[] uId = unternehmensId.toCharArray();
-//		int[] uIdNumber = compileUIdNumber(uId);
-//		if(uIdNumber.length < 9)
-//			return false;
-//		if(!checkNumber(uIdNumber))
-//			return false;
+        uid = uid.substring(4);
+        uid = uid.replace(".", "");
+        int number = 0;
+        int[] multipliers = {5, 4, 3, 2, 7, 6, 5, 4};
+        int checksum = Integer.parseInt(String.valueOf(uid.charAt(uid.length() - 1)));
+        for (int i = 0; i < uid.length() - 1; ++i) {
+            number += Integer.parseInt(String.valueOf(uid.charAt(i))) * multipliers[i];
+        }
 
-		
-		//return checkNumber(uIdNumber);
-		return true;
-	}
+        int remainder = 0;
+        if (number % 11 != 0)
+            remainder = 11 - (number % 11);
 
+        if (checksum == remainder) {
+            return true;
+        }
+
+        return false;
+    }
+
+	/*
 	private static boolean checkNumber(int[] uIdNumber) {
 		int modulo = 0;
 		int crossSum = 0;
@@ -48,6 +63,6 @@ public class UidUtil {
 			}
 		}
 		return uIdNumber;
-	}
+	}*/
 
 }
